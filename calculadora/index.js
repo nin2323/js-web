cbcc6c20e4aa55e65b5d8406023fe028893cc028
nin2323 <juansobraoperez@gmail.com>
2025-02-27 18:22:30 +0100

    calculadora en funcionamiento

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,12 +1,29 @@
-const input = document.getElementById('display');
+const input = document.getElementById('display');   // creamos dos variables, en una añadimos el valor display y en la otra el button
 const buttons  = document.querySelectorAll('button');
 
-let calculateInput = {};
+let calculateInput = '';     // creamos una variable let con un string vacio que va ir cambiando 
 
-buttons.forEach(button => {
+buttons.forEach(button => {  // recorremos la variable buttons y añadimos un evento click a todos los botones
     button.addEventListener('click', () => {
-        const value = button.textContent
+        const value = button.textContent;    // creamos una variable en la que accedemos al etxtcontent del boton
 
+        if (value === '=') {
+            try {
+                calculateInput = eval(calculateInput);  // es una función de JavaScript que toma una cadena de texto y la ejecuta como código JavaScript, asi podemos sumar, restar, etc 
+            }
+            catch {
+                calculateInput = 'error';
+            }
+        }
+
+        else if (value === 'C') {   // si el valor es C, limpia el imput
+            calculateInput = '';
+        }
+
+        else {
+            calculateInput += value;  // concatenamos los valores para que aparezcan en el imput
+        }
         
-    })
-})
+        input.value = calculateInput;
+    });
+});
